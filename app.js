87fe4a94eebbc80e228ae1b98c7544b2a822cd53
Shivam Singh <shivamsinghgbpit@gmail.com>
2021-08-20 22:19:47 +0530

    add comments

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+//requiring frameworks and files
 const express = require('express');
 const translator = require('@vitalets/google-translate-api');
 const path = require('path');
 const db = require('./config/mongoose');
-
 const TranslationModel = require('./models/translation_schema');
+
+//defining port
 const port = process.env.PORT || 5000;
+
+//initializing express app
 const app = express();
 
+//app settings
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+//app middlewares
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
+//handling routes
 app.get('/', (req, res) => {
 	return res.render('index',{
 		title: 'Text Translator',
@@ -19,7 +27,8 @@ app.get('/', (req, res) => {
 	});	
 });
 
-app.post('/', (req, res) => {	
+app.post('/', (req, res) => {
+	//finding translated text corresponding to req.body.text and req.body.language
 	TranslationModel.findOne({
 		inputText: req.body.text,
 		translationLanguage: req.body.language
@@ -29,6 +38,7 @@ app.post('/', (req, res) => {
 			return res.status(500).send(' Internal Server Error :-(');
 		}
 		if(!translation){
+			//if a translation is not found, creating new translation and saving it in db and sending it as a response 
 			translator(req.body.text, {to: req.body.language}).then((textResponse) => {
 				TranslationModel.create({
 					inputText: req.body.text,
@@ -52,7 +62,7 @@ app.post('/', (req, res) => {
 			});
 		}
 		else{
-			console.log('Found!');
+			//if a translation is found, as there is no need to store it again, returning the saved translation
 			return res.render('index', {
 				title: 'Text Translator',
 				translatedText: translation.translatedText
@@ -61,6 +71,7 @@ app.post('/', (req, res) => {
 	});
 });
 
+//listening to port
 app.listen(port, (err) => {
 	if(err){
 		console.log('Error', err);
