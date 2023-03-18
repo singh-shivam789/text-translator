30acf9a3fdc606674b2ab3be4e62527c348d8ff3
Shivam Singh <shivamsinghgbpit@gmail.com>
2023-03-18 12:31:13 +0530

    update code and some fixes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //requiring dependencies
 const express = require("express");
-const translator = require("@vitalets/google-translate-api");
+const translateAPI = require("@vitalets/google-translate-api").translate;
 const path = require("path");
-const db = require("./config/mongoose");
 const TranslationModel = require("./models/translation_schema");
 const cors = require("cors");
 //defining port
@@ -16,7 +15,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 //app middlewares
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -32,54 +31,36 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
-  //finding translated text corresponding to req.body.text and req.body.language
-  TranslationModel.findOne(
-    {
+app.post("/", async (req, res) => {
+  try {
+    let translation = await TranslationModel.findOne({
       inputText: req.body.text,
       translationLanguage: req.body.language,
-    },
-    (err, translation) => {
-      if (err) {
-        console.log("Error", err);
-        return res.status(500).send(" Internal Server Error :-(");
-      }
-      if (!translation) {
-        //if a translation is not found, creating new translation and saving it in db and sending it as a response
-        translator(req.body.text, { to: req.body.language })
-          .then((textResponse) => {
-            TranslationModel.create(
-              {
-                inputText: req.body.text,
-                translatedText: textResponse.text,
-                translationLanguage: req.body.language,
-              },
-              (err) => {
-                if (err) {
-                  console.log("Error", err);
-                  return res.status(500).send(" Internal Server Error :-(");
-                } else {
-                  return res.render("index", {
-                    title: "Text Translator",
-                    translatedText: textResponse.text,
-                  });
-                }
-              }
-            );
-          })
-          .catch((err) => {
-            console.log("Error", err);
-            return res.status(500).send(" Internal Server Error :-(");
-          });
-      } else {
-        //if a translation is found, as there is no need to store it again, returning the saved translation
-        return res.render("index", {
-          title: "Text Translator",
-          translatedText: translation.translatedText,
-        });
-      }
+    });
+    if (!translation) {
+      let textResponse = await translateAPI(req.body.text, {
+        to: req.body.language,
+      });
+      await TranslationModel.create({
+        inputText: req.body.text,
+        translatedText: textResponse.text,
+        translationLanguage: req.body.language,
+      });
+      return res.render("index", {
+        title: "Text Translator",
+        translatedText: textResponse.text,
+      });
+    } else {
+      //if a translation is found, as there is no need to store it again, returning the saved translation
+      return res.render("index", {
+        title: "Text Translator",
+        translatedText: translation.translatedText,
+      });
     }
-  );
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(500).send(" Internal Server Error :-(");
+  }
 });
 
 //listening to port
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,15 @@
 //requiring mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-let password = encodeURIComponent('singh123');
-mongoose.connect(`mongodb+srv://shivam:${password}@cluster0.p1bac6v.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
-	.then(() => {
-		console.log('Connected to MongoDB');
-	}).catch((err) => {
-		console.log(err);
-	});
+let password = encodeURIComponent("singh123");
+mongoose
+  .connect(
+    `mongodb+srv://shivam:${password}@cluster0.p1bac6v.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
